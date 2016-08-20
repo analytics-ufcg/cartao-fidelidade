18fddeb98cb34a56c46ad7c1be01365a62bf4787
Daniel Fireman <danielfireman@gmail.com>
2016-08-20 15:13:05 -0300

    Finalizing Fornecedor Model

--- src/main/java/br/edu/ufcg/analytics/Fornecedor.java
+++ src/main/java/br/edu/ufcg/analytics/Fornecedor.java
@@ -1,9 +1,18 @@
 package br.edu.ufcg.analytics;
 
+import java.util.List;
+
 /**
  * "Fornecedor" entity data holder.
  */
 public class Fornecedor {
 	public String id;
 	public String cnpjCpf;
+	public String nome;
+	public String atividadeEconomica;
+	public int anoInicial;
+	public int anoFinal;
+	public int qtdLicitacoes;
+	public double valorTotal;
+	public List<Fidelidade> fidelidade;
 }
--- src/main/java/br/edu/ufcg/analytics/FornecedorHandler.java
+++ src/main/java/br/edu/ufcg/analytics/FornecedorHandler.java
@@ -1,5 +1,7 @@
 package br.edu.ufcg.analytics;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import org.jooby.Result;
@@ -28,7 +30,33 @@ public class FornecedorHandler {
 		}
 		String type = parts[0];
 		String value = parts[1];
+		
+		// Creating a dummy Fornecedor.
+		// TODO: Remove and fetch data from database.
 		Fornecedor f = new Fornecedor();
+		f.nome = "Super Dragon Ball Z";
+		f.atividadeEconomica = "Topa Tudo";
+		f.anoInicial = 2008;
+		f.anoFinal = 2016;
+		f.qtdLicitacoes = (new Random()).nextInt(1000);
+		f.valorTotal = (new Random()).nextDouble();
+		List<Fidelidade> fidelidade = new LinkedList<>();
+		fidelidade.add(new Fidelidade(
+				"PMDB",
+				(new Random()).nextInt(1000),
+				(new Random()).nextInt(1000),
+				(new Random()).nextDouble()));
+		fidelidade.add(new Fidelidade(
+				"PT",
+				(new Random()).nextInt(1000),
+				(new Random()).nextInt(1000),
+				(new Random()).nextDouble()));
+		fidelidade.add(new Fidelidade(
+				"PSDB",
+				(new Random()).nextInt(1000),
+				(new Random()).nextInt(1000),
+				(new Random()).nextDouble()));
+		f.fidelidade = fidelidade;
 		switch (type) {
 		case "id":
 			f.id = value;
