d9a957fb34bf1ba6545d449350e56ca9371366ae
Fabio Morais <fabio.jorge@gmail.com>
2016-08-20 22:04:29 -0300

    Nova tentativa de coligações

--- src/analise-dados/analise_coligacao.R
+++ src/analise-dados/analise_coligacao.R
@@ -4,23 +4,14 @@ canditatos = c()
 
 for (ano in c(2008, 2012)) {
   
-  df_candidato  = read.csv(file = "../../dados/consulta_cand_2008_PB.txt", sep = ";", header = F, encoding = "latin1")
+  df_candidato            = read.csv(file = paste("../../dados/consulta_cand_", ano, "_PB.txt", sep = ""), sep = ";", header = F, encoding = "latin1", row.names = NULL, stringsAsFactors = FALSE)
+  df_candidato            = df_candidato[ , c(3, 10, 11, 15, 19, 20, 23, 43)]
+  colnames(df_candidato)  = c("ano_eleicao", "cargo", "nome", "nome_fantasia", "parido", "partido_extenso", "coligacao", "resultado")
 
-  tail(df_candidato)
-
-  summary(df_candidato$V20)
-  
-  
-  df_candidato  = read.csv(file = "../../dados/consulta_legendas_2008_PB.txt", sep = ";", header = F, encoding = "latin1")
-
-    
-  df_candidato  = df_candidato[ , c(3, 10, 11, 15, 19, 20, 23, 43)]
-  colnames(df_candidato) = c("ano_eleicao", "cargo", "nome", "nome_fantasia", "parido", "partido_extenso", "coligacao", "resultado")
-    
   df_candidato  = df_candidato %>% filter(resultado == "ELEITO")
-  df_candidato$coligacao[df_candidato$coligacao == "#NE#"] = NA
-  
   canditatos    = rbind(canditatos, df_candidato)
+  
 }
 
-summary(canditatos$coligacao)
+head(canditatos)
+unique(canditatos$coligacao)
